2a46a0885ff792b5b2bfacb5bcd8b78f6d2cb169
Cao Trong Phuoc <caotrongphuoc@gmail.com>
2026-08-15 21:52:56 +0700

    [REMOVE] drop leftover lib/ split (was superseded)

--- lib/menu.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Pure-bash arrow-key menu helpers.
-# Filled in during Part 4.
-
-# ----- public API (to be implemented) -----------------------------------------
-# menu_pick_one <title> <current> <item...>   -> prints picked item
-# menu_toggle   <title> <current_bool>        -> prints "true" or "false"
-# menu_main                                   -> renders the top-level menu
